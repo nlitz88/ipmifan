46ab9abdf5a1b1fec0de6c91ac1bc75913838c5f
nlitz88 <nlitz88@gmail.com>
2021-06-27 06:57:15 +0000

    Planned new method of zone delegation

--- src/interface.py
+++ src/interface.py
@@ -32,24 +32,30 @@ for device in devicesFile["disks"]:
     devLocation = check_output(["lsblk -o NAME,SERIAL | grep %s | cut -c 1-3" % diskSerial], shell=True, universal_newlines=True)
     device["devLocation"] = "/dev/" + str(devLocation).rstrip()
 
-# To avoid loading every request, initialize Zone and DeviceGroup objects.
+# To avoid loading every request, initialize Zone and DeviceGroup dictionaries.
 # NOTE: that the need for these could be eliminated if someday a database was used.
 
-deviceGroups = []
-zones = []
+deviceGroups = {}
+zones = {}
 
-# Instantiate DeviceGroup and Zone objects for those specified in deviceGroupsFile and zonesFile.
-# For every device group in the devicegroup config file list, create a new DeviceGroup instance and add it to the list.
+# For each entry in devicegroups.yaml, create a new dict
 for deviceGroup in deviceGroupsFile:
-    deviceGroups.append(DeviceGroup(deviceGroup))
+    # Create new key for devicegroup, whose value is an array of device dictionary objects.
+    deviceGroups[deviceGroup] = []
 for zone in zonesFile:
-    zones.append(Zone(zone))
+    # Create new key for zone, whose value is an array of devicegroup dictionary objects.
+    zones[zone] = []
 
 # For every device in devicesFile, add it to to the device list of the DeviceGroup that it has been assigned.
 for grouping in devicesFile:
     for device in devicesFile[grouping]:
         if "device_group" not in device:
             device["device_group"] = "default"
-        
-        if "zone" not in device:
-            device["zone"] = "default"
+        deviceGroups[device["device_group"]].append(device)
+
+
+        # NOTE: CHANGE THIS: Change devices.yaml to not include zone. Rather, include zone field in each diskgroup entry
+        #       in devicegroups.yaml, as you assign device_groups to zones, not devices!
+        # if "zone" not in device:
+        #     device["zone"] = "default"
+        # zones[device["zone"]].
