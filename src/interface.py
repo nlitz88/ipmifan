92ae2730671cf0f998aa04f88fae2d6d175e4884
nlitz88 <nlitz88@gmail.com>
2021-06-27 05:14:08 +0000

    Added functionality for mapping disks to dev locations via serial number

--- src/interface.py
+++ src/interface.py
@@ -1,16 +1,16 @@
 import yaml
-from subprocess import PIPE, run
+from subprocess import check_output
 
 # Specify path to configuration files.
-devicesFilePath = "../conf/devices.yaml"
-deviceTypeConfigFilePath = "../conf/device_type_config.yaml"
+devicesFilePath = "/home/nlitz88/ipmifan/conf/devices.yaml"
+deviceTypeConfigFilePath = "/home/nlitz88/ipmifan/conf/device_type_config.yaml"
 
 # Load configuration files as dictionaries.
 with open(devicesFilePath) as f:
-    devices = yaml.load(f)
+    devices = yaml.safe_load(f)
 
 with open(deviceTypeConfigFilePath) as f:
-    deviceTypeConfigs = yaml.load(f)
+    deviceTypeConfigs = yaml.safe_load(f)
 
 # To avoid loading every request, initialize Zone and Diskgroup objects.
 # Note that the need for these could be eliminated if someday a database was used.
@@ -20,16 +20,12 @@ zones = []
 
 
 # Map drives by serial numbers to their current name (/dev/sd*). Add this key to the devices dictionary.
-testOutput = subprocess.run("lsblk -o NAME,SERIAL", capture_output=True)
-print(testOutput)
-
-# first, grab the output of lsblk with serial and name listed.
-
-# Go through this output in the for loop below and add the mountpoint to the dictionary.
-
 for device in devices["disks"]:
-    
-    print(device["serial_number"])
+    diskSerial = device["serial_number"]
+    devLocation = check_output(["lsblk -o NAME,SERIAL | grep %s | cut -c 1-3" % diskSerial], shell=True, universal_newlines=True)
+    device["devLocation"] = "/dev/" + str(devLocation).rstrip()
+
+
 
 # initialize DiskGroup objects
 
